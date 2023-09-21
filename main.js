d585c30ede6cd8195c2336d09afcb4119df4e703
Lynjai Jimenez <139661024+IntermediaDesigns@users.noreply.github.com>
2023-09-20 21:40:35 -0400

    1

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 // Clear screen function
 function clearScreen() {
        document.getElementById("result").value = "";
-   }
+}
     
-   // Display calculation function
-   function display(value) {
+// Display calculation function
+function display(value) {
        document.getElementById("result").value += value;
-   }
+}
     
-   // This function evaluates the expression and returns the result
-   function calculate() {
+// This function evaluates the expression and returns the result
+function calculate() {
        var p = document.getElementById("result").value;
        var q = eval(p);
        document.getElementById("result").value = q;
-   }
+}
